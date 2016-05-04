14442b9f12e65eae6a56c5b346c2478c6c9a2957
Lloyd Brown <lloyd_brown@byu.edu>
2016-05-04 17:18:05 -0600

    clearing out some debug stuff, to keep sketch small enough to fit

--- rotator_features.h
+++ rotator_features.h
@@ -156,9 +156,9 @@
 
 
 
-#define DEFAULT_DEBUG_STATE 1 // 1 = activate debug mode at startup; this should be set to zero unless you're debugging something at startup
+#define DEFAULT_DEBUG_STATE 0 // 1 = activate debug mode at startup; this should be set to zero unless you're debugging something at startup
 
-#define DEBUG_DUMP  // normally compile with this activated unless you're really trying to save memory
+// #define DEBUG_DUMP  // normally compile with this activated unless you're really trying to save memory
 // #define DEBUG_LOOP
 // #define DEBUG_MEMORY
 // #define DEBUG_BUTTONS
