ad2687868b1529dd1a91442bd9ea68588e641298
Lloyd Brown <somewhere.or.other@gmail.com>
2016-05-11 08:58:06 -0600

    assigning pins for various features

--- rotator_pins.h
+++ rotator_pins.h
@@ -18,8 +18,8 @@
 #define rotate_cw_ccw_pwm 0      // optional - PWM on CW and CCW output - set to 0 to disable (must be PWM capable pin)
 #define rotate_cw_freq 0         // optional - CW variable frequency output
 #define rotate_ccw_freq 0        // optional - CCW variable frequency output
-#define button_cw 0              // normally open button to ground for manual CW rotation (schematic pin: A1)
-#define button_ccw 0             // normally open button to ground for manual CCW rotation (schematic pin: A2)
+#define button_cw 2              // normally open button to ground for manual CW rotation (schematic pin: A1)
+#define button_ccw 3             // normally open button to ground for manual CCW rotation (schematic pin: A2)
 #define serial_led 0             // LED blinks when command is received on serial port (set to 0 to disable)
 #define rotator_analog_az A0     // reads analog azimuth voltage from rotator - pin 4 on Yaesu connector
 #define azimuth_speed_voltage 0  // optional - PWM output for speed control voltage feed into rotator (on continually unlike rotate_cw_pwm and rotate_ccw_pwm)
@@ -28,7 +28,7 @@
 #define az_speed_pot 0           // connect to wiper of 1K to 10K potentiometer for speed control (set to 0 to disable)
 #define az_preset_pot 0          // connect to wiper of 1K to 10K potentiometer for preset control (set to 0 to disable)
 #define preset_start_button 0    // connect to momentary switch (ground on button press) for preset start (set to 0 to disable or for preset automatic start)
-#define button_stop 0            // connect to momentary switch (ground on button press) for preset stop (set to 0 to disable or for preset automatic start)
+#define button_stop 10            // connect to momentary switch (ground on button press) for preset stop (set to 0 to disable or for preset automatic start)
 #define rotation_indication_pin 0
 #define blink_led 0
 #define az_stepper_motor_pulse 44 //0
@@ -46,8 +46,8 @@
   #define rotate_up_freq 0          // optional - UP variable frequency output
   #define rotate_down_freq 0        // optional - UP variable frequency output
   #define rotator_analog_el A1      // reads analog elevation voltage from rotator
-  #define button_up 0               // normally open button to ground for manual up elevation
-  #define button_down 0             // normally open button to ground for manual down rotation
+  #define button_up 4               // normally open button to ground for manual up elevation
+  #define button_down 5             // normally open button to ground for manual down rotation
   #define brake_el 0                // goes high to disengage elevation brake (set to 0 to disable)
   #define elevation_speed_voltage  0 // optional - PWM output for speed control voltage feed into rotator (on continually unlike rotate_up_pwm and rotate_down_pwm)
   #define el_stepper_motor_pulse 0
@@ -85,7 +85,7 @@
 #endif                                                // read http://arduino.cc/en/Reference/AttachInterrupt for details on hardware and interrupts
 
 #ifdef FEATURE_PARK
-  #define button_park 0
+  #define button_park 11
 #endif
 
 //classic 4 bit LCD pins
